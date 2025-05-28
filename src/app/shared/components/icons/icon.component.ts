8556df59b8f2dfd3789f711d273bd3d9cca17611
uesleinasch <uesleinac@gmail.com>
2025-05-28 13:54:18 -0300

    icon component

---
+++ src/app/shared/components/icons/icon.component.ts
@@ -0,0 +1,14 @@
+import { Component, input } from '@angular/core';
+import { IconsLibrary } from './model/icons.type';
+
+@Component({
+  selector: 'app-icon',
+  standalone: true,
+  imports: [],
+  templateUrl: './icon.component.html',
+  styleUrl: './icon.component.css',
+})
+export class IconsComponent {
+  size = input<string>('1rem');
+  icon = input<IconsLibrary>();
+}
