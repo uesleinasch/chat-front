8f5072f3ecc355d6b36710c5dc578ebf44532800
uesleinasch <uesleinac@gmail.com>
2025-05-28 13:54:48 -0300

    header layout

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
-
+import {MatIconModule} from '@angular/material/icon';
+import { UserPicComponent } from "../../shared/components/user-pic/user-pic.component";
+import { NgIcon } from '@ng-icons/core';
+import { IconsComponent } from "../../shared/components/icons/icon.component";
+import { ButtonComponent } from "../../shared/components/button/button.component";
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule, UserPicComponent, NgIcon, IconsComponent, ButtonComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
+  async openCamera() {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({ 
+        video: true, 
+        audio: false 
+      });
+      console.log('Câmera aberta com sucesso', stream);
+    } catch (error) {
+      console.error('Erro ao acessar a câmera:', error);
+      
+      if ((error as any).name === 'NotAllowedError') {
+        alert('Permissão de câmera negada. Verifique as configurações do navegador.');
+      } else if ((error as any).name === 'NotFoundError') {
+        alert('Nenhuma câmera encontrada no dispositivo.');
+      } else {
+        alert('Erro ao acessar a câmera.');
+      }
+    }
+  }
 }
