08db22f5410e09262b5de6617d77ea7e2dbd0530
uesleinasch <uesleinac@gmail.com>
2025-05-28 13:54:55 -0300

    configs aicons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { BodyComponent } from "./layout/body/body.component";
-import { FooterComponent } from "./layout/footer/footer.component";
-import { HeaderComponent } from "./layout/header/header.component";
-
+import { BodyComponent } from './layout/body/body.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { featherAirplay } from '@ng-icons/feather-icons';
+import { heroUsers } from '@ng-icons/heroicons/outline';
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, BodyComponent, FooterComponent, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'chat-front';
 }
+
