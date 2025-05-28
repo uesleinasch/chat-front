a21a1e7198c64075cc233f9933bc98d449ee30df
uesleinasch <uesleinac@gmail.com>
2025-05-28 13:54:00 -0300

    chat send component

---
+++ src/app/features/chat-send/chat-send.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-send',
+  standalone: true,
+  imports: [],
+  templateUrl: './chat-send.component.html',
+  styleUrl: './chat-send.component.css'
+})
+export class ChatSendComponent {
+
+}
